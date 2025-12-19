a5893b2f1d8342dc690d42ed2ba54452a9cadb4a
Juan Camilo Sánchez <90232541+JuanCSanchezH@users.noreply.github.com>
2025-12-18 21:18:21 -0500

    feature: add scrapy structure and configuration (#2)
    * feature: add scrapy structure and configuration
    
    * fix: change item pipelines path

---
+++ obituaries/settings.py
@@ -0,0 +1,10 @@
+BOT_NAME = "obituaries"
+
+SPIDER_MODULES = ["obituaries.spiders"]
+NEWSPIDER_MODULE = "obituaries.spiders"
+
+ROBOTSTXT_OBEY = True
+
+ITEM_PIPELINES = {
+    "obituaries.pipelines.echovita_scraper.ObituariesPipeline": 300,
+}
